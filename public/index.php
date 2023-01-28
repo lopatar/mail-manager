35002cb7a38f776048967026a73f7c9b9ca7bf94
Jiří Lopatář <github@lopatar.me>
2023-01-28 20:18:20 +0100

    add basic auth

--- public/index.php
+++ public/index.php
@@ -6,11 +6,20 @@ require __DIR__ . '/../vendor/autoload.php';
 use App\Middleware\HtmlHeader;
 use App\SdkConfig;
 use Sdk\App;
+use Sdk\Middleware\HttpBasicAuth;
 
 $config = new SdkConfig();
 $app = new App($config);
 
+$users = [
+    ['username' => 'test', 'password' => '$2y$12$3W.teM8Ph5cp4CwZy9r0D.MtI.RYLW0kSsYpfvTrBio8tBevQCG2m']
+];
+
+
+$basicAuth = new HttpBasicAuth($users);
 $htmlHeader = new HtmlHeader();
+
+$app->addMiddleware($basicAuth);
 $app->addMiddleware($htmlHeader);
 
 $app->view('/', 'Home.html');
