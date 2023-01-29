c043de9886bfb8128200fed60391b0034b29da74
Jiří Lopatář <github@lopatar.me>
2023-01-29 15:35:11 +0100

    debug test

--- App/Models/PermanentAccount.php
+++ App/Models/PermanentAccount.php
@@ -34,6 +34,7 @@ final readonly class PermanentAccount implements IMailAccount
      */
     public static function getAll(): array
     {
+        echo '/usr/bin/getent group mail | ' . "/usr/bin/awk -F ':' '{print $4}'";
         $commandOutput = SysCommand::runString('/usr/bin/getent group mail | ' . "/usr/bin/awk -F ':' '{print $4}'");
         $groupAccounts = explode(',', $commandOutput);
 
