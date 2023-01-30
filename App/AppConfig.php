05caeb5a7fbec63307bcbac5e650b1399935dfaf
Jiří Lopatář <github@lopatar.me>
2023-01-30 20:39:32 +0100

    add example appconfig values

--- App/AppConfig.php
+++ App/AppConfig.php
@@ -5,8 +5,8 @@ namespace App;
 
 abstract class AppConfig
 {
-    public const EMAIL_DOMAIN = 'lopatar.me';
-    public const ROUNDCUBE_LINK = 'https://mail.lopatar.me/';
+    public const EMAIL_DOMAIN = 'example.com';
+    public const ROUNDCUBE_LINK = 'https://wemail.example.com/';
     public const MAX_TEMPORARY_EMAIL_MINUTES = 360;
     public const DEFAULT_TIMEZONE = 'Europe/Prague';
 }
